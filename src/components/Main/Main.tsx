28f6418ab70920bc0475aeae3392d0813298bf38
Max Ryaguzov <ryaguzovmaks@yandex.ru>
2022-06-01 10:56:41 +0400

    remove routes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,12 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Converter from './Converter'
 
 export default function Main() {
 	return (
 		<BrowserRouter basename='/currency_converter'>
-			<Routes>
-				<Route path='/' element={<Converter />} />
-			</Routes>
+			<Converter />
 		</BrowserRouter>
 	)
 }
